74bfce00fc264e57436951999d81d4c3e9da9923
DeveshKumarTripathi <devesh8245@gmail.com>
2025-01-07 13:54:56 +0530

    Update ReverseString.java

--- Reverse-Ques/ReverseString.java
+++ Reverse-Ques/ReverseString.java
@@ -1,1 +1,8 @@
+class ReverseString {
+    public static void main(String[] args){
+        String str = "hello";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed String: " + reversed);
+    }
+}
 
