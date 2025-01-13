9e7820a2cc4faf5dbc3e97cd136b85ba8b014467
DeveshKumarTripathi <devesh8245@gmail.com>
2025-01-13 23:49:41 +0530

    Add files via upload

---
+++ Arrays and ArrayList/Arraylist.java
@@ -0,0 +1,23 @@
+import  java.util.ArrayList;
+import java.util.Scanner;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+//                list.add(10);
+//                list.add(10);
+//                list.add(10);
+//                list.add(10);
+//                System.out.println(list);
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(list.get(i)+" ");
+            System.out.println();
+        } 
+    }
+}
