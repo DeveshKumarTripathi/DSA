778230ba47142910276f329c9647ea3f37bf354f
Devesh Kumar Tripathi <devesh8245@gmail.com>
2025-04-05 22:24:51 +0530

    Update ReverseArray.java

--- Reverse-Ques/ReverseArray.java
+++ Reverse-Ques/ReverseArray.java
@@ -4,7 +4,7 @@ class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};
 
-        int left = 0, right = arr.length - 1;
+        int left = 0, right = arr.length - 1; 
         while (left < right) {
             int temp = arr[left];
             arr[left] = arr[right];
